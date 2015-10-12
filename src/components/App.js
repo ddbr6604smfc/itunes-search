35c226b59862ae5564d8da0230555210dd1a85e2
ddbr6604smfc <ddbr6604smfc@gmail.com>
2015-10-12 16:45:36 -0500

    implement basic track search

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import jsonp from 'jsonp-p';
 
-const App = () => <h1>Hello, World!</h1>;
+const search = query =>
+  jsonp(`https://itunes.apple.com/search?media=music&entity=song&limit=20&term=${query}`)
+    .then(response => response.results);
 
-export default App;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: '',
+      results: [],
+    };
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+
+    const query = this.state.query.trim();
+
+    if (query) {
+      search(this.state.query).then(results => {
+        this.setState({ results });
+      });
+    }
+  }
+
+  onChange = (event) => {
+    this.setState({
+      query: event.target.value,
+    });
+  }
+
+  render() {
+    const { query, results } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            placeholder="search"
+            value={query}
+            onChange={this.onChange}
+          />
+          <input type="submit" value="Search" />
+        </form>
+
+        <h1>Search results for {query}:</h1>
+
+        <ul>
+          {
+            results.map(result =>
+              <li key={result.trackId}>
+                <img src={result.artworkUrl60} alt={result.trackName} />
+                <p>{result.trackName}</p>
+                <p>{result.artistName} - {result.collectionName}</p>
+              </li>
+            )
+          }
+        </ul>
+      </div>
+    );
+  }
+}
